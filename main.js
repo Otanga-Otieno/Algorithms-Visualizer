27fa342730f08aa1e24ffa1ec194546f0714cce0
Otanga-Otieno <otieno.otanga@gmail.com>
2022-01-31 14:21:11 +0300

    Allow user array

--- main.js
+++ main.js
@@ -1,22 +1,55 @@
 import { bubbleSort, selectionSort } from "./Algorithms/sort/algorithms.js";
 
 var arr = [54, 5, 92, 49, 23, 8];
+//var arr = [20, 3, 15, 4, 6];
 
 function originalArray(arr) {
+
     let display = document.getElementById("origarray");
     let text = document.createElement("p");
     text.innerHTML = "Original Array: ";
     display.appendChild(text);
+
     let table = document.createElement("table");
     let tablerow = document.createElement("tr");
-
     display.appendChild(table).appendChild(tablerow);
+
     for(let i=0; i<arr.length; i++) {
         let tablecell = document.createElement("td");
         tablecell.innerHTML = arr[i];
         tablerow.appendChild(tablecell);
     }
+
 }
+
+function clearChildren(id) {
+
+    let element = document.getElementById(id);
+    while(element.firstChild) {
+        element.removeChild(element.lastChild);
+    }
+
+}
+
+function getArray() {
+
+    let input = document.getElementById("userarray").value;
+    clearChildren("origarray");
+    let arr = input.substring(1, input.length-1).split(",");
+    arr = arr.map(Number);
+    originalArray(arr);
+    printArray(arr);
+
+}
+
+function printArray(arr) {
+
+    clearChildren("arrtable");
+    console.log(bubbleSort(arr));
+
+}
+
+document.getElementById("getarrbtn").addEventListener("click", getArray);
 
 originalArray(arr);
 
