876713cf29a003ffa44638256297c5689d6d5fd1
Otanga-Otieno <otieno.otanga@gmail.com>
2022-03-11 15:28:29 +0300

    Style landing page

--- index/src/App.js
+++ index/src/App.js
@@ -9,11 +9,11 @@ function App() {
       <h2>Sort Algorithms</h2>
 
       <ol style={{listStylePosition: "inside"}}>
-        <li><Link to={"bubblesort"}>Bubble Sort</Link></li>
-        <li><Link to={"heapsort"}>Heap Sort</Link></li>
-        <li><Link to={"insertionsort"}>Insertion Sort</Link></li>
-        <li><Link to={"quicksort"}>Quick Sort</Link></li>
-        <li><Link to={"selectionsort"}>Selection Sort</Link></li>
+        <div className='rounded shadow p-3 m-3 bg-white'><i className='fa-solid fa-droplet-degree'></i><Link to={"bubblesort"}>Bubble Sort</Link></div>
+        <div className='rounded shadow p-3 m-3 bg-white'><Link to={"heapsort"}>Heap Sort</Link></div>
+        <div className='rounded shadow p-3 m-3 bg-white'><Link to={"insertionsort"}>Insertion Sort</Link></div>
+        <div className='rounded shadow p-3 m-3 bg-white'><Link to={"quicksort"}>Quick Sort</Link></div>
+        <div className='rounded shadow p-3 m-3 bg-white'><Link to={"selectionsort"}>Selection Sort</Link></div>
       </ol>
 
     </div>
