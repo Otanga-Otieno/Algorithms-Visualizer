cdfd9c8311a54e06103f07f21c592bee2ee4159b
Otanga-Otieno <otieno.otanga@gmail.com>
2022-03-17 21:23:16 +0300

    Change div margins

--- index/src/App.js
+++ index/src/App.js
@@ -9,11 +9,11 @@ function App() {
       <h2>Sort Algorithms</h2>
 
       <ol style={{listStylePosition: "inside"}}>
-        <div className='rounded shadow p-3 m-3 bg-white'><i className='fas fa-droplet fa-2x mx-5'></i><Link to={"bubblesort"}>Bubble Sort</Link></div>
-        <div className='rounded shadow p-3 m-3 bg-white'><i className='fas fa-tree fa-2x mx-5'></i><Link to={"heapsort"}>Heap Sort</Link></div>
-        <div className='rounded shadow p-3 m-3 bg-white'><i className='fas fa-arrow-right-to-bracket fa-2x mx-5'></i><Link to={"insertionsort"}>Insertion Sort</Link></div>
-        <div className='rounded shadow p-3 m-3 bg-white'><i className='fas fa-jet-fighter fa-2x mx-5'></i><Link to={"quicksort"}>Quick Sort</Link></div>
-        <div className='rounded shadow p-3 m-3 bg-white'><i className='fas fa-arrow-up-short-wide fa-2x mx-5'></i><Link to={"selectionsort"}>Selection Sort</Link></div>
+        <div className='rounded shadow p-3 my-3 bg-white'><i className='fas fa-droplet fa-2x mx-5'></i><Link to={"bubblesort"}>Bubble Sort</Link></div>
+        <div className='rounded shadow p-3 my-3 bg-white'><i className='fas fa-tree fa-2x mx-5'></i><Link to={"heapsort"}>Heap Sort</Link></div>
+        <div className='rounded shadow p-3 my-3 bg-white'><i className='fas fa-arrow-right-to-bracket fa-2x mx-5'></i><Link to={"insertionsort"}>Insertion Sort</Link></div>
+        <div className='rounded shadow p-3 my-3 bg-white'><i className='fas fa-jet-fighter fa-2x mx-5'></i><Link to={"quicksort"}>Quick Sort</Link></div>
+        <div className='rounded shadow p-3 my-3 bg-white'><i className='fas fa-arrow-up-short-wide fa-2x mx-5'></i><Link to={"selectionsort"}>Selection Sort</Link></div>
       </ol>
 
     </div>
