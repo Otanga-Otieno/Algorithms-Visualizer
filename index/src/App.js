e75d4df0aa922d8dd70f52ce3f560952bd56569d
Otanga-Otieno <otieno.otanga@gmail.com>
2022-03-18 20:37:23 +0300

    Center align algorithm names vertically

--- index/src/App.js
+++ index/src/App.js
@@ -8,13 +8,15 @@ function App() {
 
       <h2>Sort Algorithms</h2>
 
-      <ol style={{listStylePosition: "inside"}}>
-        <div className='rounded shadow p-3 my-3 bg-white'><i className='fas fa-droplet fa-2x mx-5'></i><Link to={"bubblesort"}>Bubble Sort</Link></div>
-        <div className='rounded shadow p-3 my-3 bg-white'><i className='fas fa-tree fa-2x mx-5'></i><Link to={"heapsort"}>Heap Sort</Link></div>
-        <div className='rounded shadow p-3 my-3 bg-white'><i className='fas fa-arrow-right-to-bracket fa-2x mx-5'></i><Link to={"insertionsort"}>Insertion Sort</Link></div>
-        <div className='rounded shadow p-3 my-3 bg-white'><i className='fas fa-jet-fighter fa-2x mx-5'></i><Link to={"quicksort"}>Quick Sort</Link></div>
-        <div className='rounded shadow p-3 my-3 bg-white'><i className='fas fa-arrow-up-short-wide fa-2x mx-5'></i><Link to={"selectionsort"}>Selection Sort</Link></div>
-      </ol>
+      <div>
+        <ol style={{listStylePosition: "inside"}}>
+          <div className='rounded shadow p-3 my-3 bg-white' style={{display: "flex", alignItems: "center", justifyContent: "center"}} ><i className='fas fa-droplet fa-2x mx-5'></i><Link to={"bubblesort"}>Bubble Sort</Link></div>
+          <div className='rounded shadow p-3 my-3 bg-white' style={{display: "flex", alignItems: "center", justifyContent: "center"}} ><i className='fas fa-tree fa-2x mx-5'></i><Link to={"heapsort"}>Heap Sort</Link></div>
+          <div className='rounded shadow p-3 my-3 bg-white' style={{display: "flex", alignItems: "center", justifyContent: "center"}} ><i className='fas fa-arrow-right-to-bracket fa-2x mx-5'></i><Link to={"insertionsort"}>Insertion Sort</Link></div>
+          <div className='rounded shadow p-3 my-3 bg-white' style={{display: "flex", alignItems: "center", justifyContent: "center"}} ><i className='fas fa-jet-fighter fa-2x mx-5'></i><Link to={"quicksort"}>Quick Sort</Link></div>
+          <div className='rounded shadow p-3 my-3 bg-white' style={{display: "flex", alignItems: "center", justifyContent: "center"}} ><i className='fas fa-arrow-up-short-wide fa-2x mx-5'></i><Link to={"selectionsort"}>Selection Sort</Link></div>
+        </ol>
+      </div>
 
     </div>
   );
