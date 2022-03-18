eb2a9d020ff0cbb9667c61774b4fff3e80ee3534
Otanga-Otieno <otieno.otanga@gmail.com>
2022-03-18 21:05:46 +0300

    Change list to div

--- index/src/App.js
+++ index/src/App.js
@@ -9,13 +9,13 @@ function App() {
       <h2>Sort Algorithms</h2>
 
       <div>
-        <ol style={{listStylePosition: "inside"}}>
+        <div style={{listStylePosition: "inside"}}>
           <div className='rounded shadow p-2 my-3 bg-white' > <i className='fas fa-droplet fa-2x mx-5'></i> <div><Link to={"bubblesort"}>Bubble Sort</Link></div> </div>
           <div className='rounded shadow p-2 my-3 bg-white' > <i className='fas fa-tree fa-2x mx-5'></i> <div><Link to={"heapsort"}>Heap Sort</Link></div> </div>
           <div className='rounded shadow p-2 my-3 bg-white' > <i className='fas fa-arrow-right-to-bracket fa-2x mx-5'></i> <div><Link to={"insertionsort"}>Insertion Sort</Link></div> </div>
           <div className='rounded shadow p-2 my-3 bg-white' > <i className='fas fa-jet-fighter fa-2x mx-5'></i> <div><Link to={"quicksort"}>Quick Sort</Link></div> </div>
           <div className='rounded shadow p-2 my-3 bg-white' > <i className='fas fa-arrow-up-short-wide fa-2x mx-5'></i> <div><Link to={"selectionsort"}>Selection Sort</Link></div> </div>
-        </ol>
+        </div>
       </div>
 
     </div>
